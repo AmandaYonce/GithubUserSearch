0aaac43ed72e9bc891a3784bf6b0f52f4bcb92b7
Amanda Yonce <amanda.stork@gmail.com>
2020-07-08 12:17:23 -0400

    created repo

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*
-1. Get Location
-    -use geolocation api to get coordinates or use fallback location
-2. Get Photo info from Flickr
-    -use fetch to send get request to flickr
-    -include the lat and long
-    -include a search term
-
-    -process the promises to get the photo data
-    -convert json to a usable object
-    -send the photo data to a display function
-3. Display Photos
-    -create the image urls from the photo data-see flickr documentation
-    -insert an image tag into the page
-    -create image element
-    -set source attribute to image url
-    -display link to the image's flickr page
-4. Provide a way to click through the photos
-*/
 
 /*
 Key:
@@ -42,39 +23,38 @@ var options = {         //optional parameters for geolocation search
     displayPhoto(blockLocation);
   }
 
+
 const blockLocation={latitude: 37, longitude: 14}       //fallback location
 let imageUrlArray=[]
 
 function displayPhoto(crd){         //send request to flickr api in this function
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
     imageUrlArray=[]
 
     const proxy="https://shrouded-mountain-15003.herokuapp.com/"
-    const url=proxy+"https://flickr.com/services/rest/?api_key=e35c72b1e1a0100981627f0b8f995665&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&page=1&"+"lat="+crd.latitude+"&"+"lon="+crd.longitude
+    const url=proxy+"https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=e35c72b1e1a0100981627f0b8f995665&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&page=1&"+"lat="+crd.latitude+"&"+"lon="+crd.longitude
 
     fetch(url)
 
         .then(function (responseObject){
+            console.log(responseObject)
             return responseObject.json()
         })
         .then(function (data){
+            console.log(data)
             createImageUrlObject(data)
             return data
         })
+        
 }
-
 
 function createImageUrlObject(data){        //create array of photo urls
 
     for(let i=0; i<data.photos.photo.length; i++){
             imageUrlArray.push("https://farm" + data.photos.photo[i].farm+".staticflickr.com/"+data.photos.photo[i].server+"/"+data.photos.photo[i].id+"_"+data.photos.photo[i].secret+".jpg")
     }
-
     sendImageToHTML(imageUrlArray)
     return imageUrlArray
 }
-
 
 let i=0
 
